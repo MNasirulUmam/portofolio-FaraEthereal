8efa8efe54f7a2ec6a33d58147c3b65168b19bd1
M.Nasirul Umam <nasirul250400@gmail.com>
2023-06-12 16:15:39 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fara Ethereal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- My Css -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow" style ="background-color: #E9967A">
+      <div class="container">
+        <a class="navbar-brand" href="#">Fara Ethereal</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Abouts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Me</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar end -->
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="img/logo.png" alt="Fara Ethereal" width="200">
+      <h1 class="display-4">Fara Ethereal</h1>
+      <p class="lead">Event Organizer</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffff" fill-opacity="1" d="M0,128L48,160C96,192,192,256,288,250.7C384,245,480,171,576,122.7C672,75,768,53,864,85.3C960,117,1056,203,1152,240C1248,277,1344,267,1392,261.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>      
+    </section>
+    <!-- Jumbotron end -->
+    <!-- Abouts -->
+    <section id ="about">
+      <div class="container text-center mb-3">
+        <div class="row mt-5 mb-2">
+          <h2>About Me</h2>
+        </div>
+        <div class="row justify-content-center fs-5">
+          <div class="col-md-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique veritatis dolorem animi itaque, 
+              porro reiciendis deserunt ullam atque veniam impedit cupiditate accusantium numquam suscipit mollitia placeat voluptate, officia eaque assumenda.
+          </div>
+          <div class="col-md-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique veritatis dolorem animi itaque, 
+              porro reiciendis deserunt ullam atque veniam impedit cupiditate accusantium numquam suscipit mollitia placeat voluptate, officia eaque assumenda.
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffe4c4" fill-opacity="1" d="M0,128L48,160C96,192,192,256,288,250.7C384,245,480,171,576,122.7C672,75,768,53,864,85.3C960,117,1056,203,1152,240C1248,277,1344,267,1392,261.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    <!-- Abouts end -->
+
+    <!-- Ny projects -->
+    <section id="projects">
+      <div class="container mb-2" style="background-color: #ffe4c4">
+        <div class="row text-center">
+          <div class="col mb-2 mt-5">
+            <h2>Projects</h2>
+          </div>
+        </div>
+       <div class="row justify-content-center">
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/projects/1.jpg" class="card-img-top" alt="pr 01">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/projects/2.jpg" class="card-img-top" alt="pr 02">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/projects/3.jpg" class="card-img-top" alt="pr 03">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/projects/4.jpg" class="card-img-top" alt="pr 03">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div><div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/projects/6.jpg" class="card-img-top" alt="pr 03">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Ny projects end -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
