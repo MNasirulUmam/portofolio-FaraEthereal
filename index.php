8398feb3677eafb910b06e5f333400b9d18b3214
M.Nasirul Umam <nasirul250400@gmail.com>
2023-06-13 10:50:47 +0700

    add coment

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fara Ethereal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- Bootsrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Bootstrap end -->
     <!-- My Css -->
     <link rel="stylesheet" href="css/style.css">
   </head>
